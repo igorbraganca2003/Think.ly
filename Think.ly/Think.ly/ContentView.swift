2c9ddd99f82db8d026aedd83102eba6402d2bb57
Igor Bragança <igor.braganca2003@gmail.com>
2024-11-22 15:29:06 -0300

    Criando arquivo XCode

---
+++ Think.ly/Think.ly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Think.ly
+//
+//  Created by Igor Bragança Toledo on 22/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Think.ly/Think.ly/Think_lyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Think_lyApp.swift
+//  Think.ly
+//
+//  Created by Igor Bragança Toledo on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Think_lyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
